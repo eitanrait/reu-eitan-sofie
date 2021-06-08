3e12784c7d566c9332c3c378ce7b9da306776d91
eitanrait <exr644@miami.edu>
2021-06-08 00:23:48 -0500

    added randomwalk and findPath to chasing.c; needs fixing of algorithm when boat x can be at an x coordinate less than the x coordinate of boat y

--- chasing.c
+++ chasing.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
-#include "chase_static_point.h"
+#include <stdlib.h>
+#include <time.h>
+#include "classification.h"
+
+#define FALSE 0
+#define TRUE 1
+#define N .1
+#define NE .2
+#define E .3
+#define SE .4
+#define S .5
+#define SW .6
+#define W .7
+#define NW .8
+#define STAY .9
+#define GONE 1
 
 typedef struct {
   int i;
@@ -7,8 +22,57 @@ typedef struct {
   char visible[11];
 } point_t;
 
-void synthesizeChasing(int x0, int y0, int x1, int y1) {
-	// should call findLine() from chase_static_point
+/*void synthesizeChasing(int x0, int y0, int x1, int y1) {
+	// should call findPath() from chase_static_point
+        findPath(x0,y0,x1,y1);
+	}*/
+
+void findPath(point_t * point0, point_t * point1) { 
+
+   int B = point0->i - point1->i;
+   int A = point1->j - point0->j;
+   int g = 2 * A + B;	// initial biased error
+   int diag_inc = 2 * (A + B);
+   int right_inc = 2 * A; 
+   
+   if(g >= 0) {
+     // go in y direction
+     point0->j++;
+     g += diag_inc;
+   } else {	// if error is negative
+     // go in x direction
+     g += right_inc;
+   }
+   point0->i += 1;	// increment in x direction
+   
+}
+
+void randomwalk(point_t * point, float prob) {
+  if (prob >= 0 && prob < N)
+    point->j++;
+  else if (prob < NE) {
+    point->i++;
+    point->j++;
+  }
+  else if (prob < E)
+    point->i++;
+  else if ( prob < SE) {
+    point->i++;
+    point->j--;
+  }
+  else if (prob < S)
+    point->j--;
+  else if (prob < SW) {
+    point->i--;
+    point->j--;
+  }
+  else if(prob < W)
+    point->i--;
+  else if (prob < NW) {
+    point->i--;
+    point->j++;
+  }
+  else {}
 }
 
 int main() {
@@ -18,13 +82,25 @@ int main() {
 	// thus Y.i < X.i because we are using Brenenham's algorithm
 	point_t X;
 	point_t Y;
+	float prob;
+	int halt = FALSE;
 
 	Y.i = 10;
 	Y.j = 10;
-	X.i = 25;
-	X.j = 15;
+	X.i = 44;
+	X.j = 33;
+	
+	srand(time(0));
+	prob = (rand() % 100)*.01;
 
-	synthesizeChasing(Y.i, Y.j, X.i, X.j);	
+	while(halt == FALSE && Y.i < X.i) {	  
+	  findPath(&Y, &X);  
+	  randomwalk(&X, prob);
+	  prob = (rand() % 100)*.01;
+	  printf("Y.i:%d, Y.j%d, X.i:%d, X.j:%d\n", Y.i, Y.j, X.i, X.j);
+
+	}
+	
 	return 0;
 
 }
--- randomwalk.c
+++ randomwalk.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <string.h>
 #include "classification.h"
 
@@ -10,81 +9,48 @@
 #define SE .4
 #define S .5
 #define SW .6
-#define W .7 
+#define W .7
 #define NW .8
 #define STAY .9
 #define GONE 1
-#define I 210
-#define J 720
-#define STEPS 200
 
-struct point{
-  int i;
-  int j;
-  char visible[11];
-};
+void randomwalk(int x, int y, float prob) {
 
-struct point points[STEPS];
-
-int main(int argc, char* argv[]) {
-  float prob;
-  int t;
-  struct point p;
-  
-  srand(time(0));
-  prob = (rand() % 100) *.01;
-  p.i = I;
-  p.j = J;
-  
-  for(t=0;t<STEPS;t++) {
-      if (prob >= 0 && prob < N)
-	p.j++;
-      else if (prob < NE) {
-	p.i++;
-	p.j++;
-      }
-      else if (prob < E)
-	p.i++;
-      else if ( prob < SE) {
-	p.i++;
-	p.j--;
-      }
-      else if (prob < S)
-	p.j--;  
-      else if (prob < SW) {
-	p.i--;
-	p.j--;
-      }
-      else if(prob < W)
-	p.i--;
-      else if (prob < NW) {
-	p.i--;
-	p.j++;
-      }
-      else {}    
-
-      if(prob >= STAY && prob < GONE) {
-	strcpy(points[t].visible,"not visible");
-	//printf("%s , probability = %.2f\n",points[a].visible, prob);
-      }
-      else {
-	points[t].i = p.i;
-	points[t].j = p.j;
-	//printf("i= %d, j= %d, probability = %.2f\n",coordinates[t].i,coordinates[t].j, prob);
-      }
-      prob = (rand() % 100) * .01;
+  if (prob >= 0 && prob < N)
+    y++;
+  else if (prob < NE) {
+    x++;
+    y++;
   }
-
-  int index;
-
-  for (index = 0; index < STEPS; index++) {
-    if(strcmp(points[index].visible,"") == 0)
-      printf("%d,%d\n",points[index].i,points[index].j);
-    else 
-      printf("not visible\n");
+  else if (prob < E)
+    x++;
+  else if ( prob < SE) {
+    x++;
+    y--;
   }
+  else if (prob < S)
+    y--;  
+  else if (prob < SW) {
+    x--;
+    y--;
+  }
+  else if(prob < W)
+    x--;
+  else if (prob < NW) {
+    x--;
+    y++;
+  }
+  else {}    
   
-  printf("\n");
-
+/*  
+  p.region = inRegion(p.i,p.j);
+  
+  if(prob >= STAY && prob < GONE)
+    p.region = 'i';
+  
+  points[t].i = p.i;
+  points[t].j = p.j;
+  points[t].region = p.region; 
+*/
   return 0;
 }
