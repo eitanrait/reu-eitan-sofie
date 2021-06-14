b7401d2a7b7915d2233a2c1e17bbb69833491c22
SofKard <sofkard@DESKTOP-E7KFGNP.localdomain>
2021-06-14 16:58:19 -0500

    added a following.c file where we should write the following algo

---
+++ following.c
@@ -0,0 +1,13 @@
+// start writing fillowing algo here
+//
+// what does it mean to be behind X?
+// Boat Y is behind X when Y is behind X for a certain amount of time
+// Y could just be driving by X for a bit (that is not following)
+// need to check for the amount of time
+//
+#include <stdio.h>
+
+int main() {
+	
+	return 0;
+}
