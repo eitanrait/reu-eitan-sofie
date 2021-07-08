a51fd783b27e23b90bab879073480fd0226da581
eitanrait <exr644@miami.edu>
2021-07-07 20:42:08 -0400

    added 2nd approach files for chasing and following

---
+++ chasing_prob.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include <stdbool.h>
+#include "diver.h"
+
+struct Point points[SIZE];
+
+void chase(struct Params * params, struct Point * u, struct Point * v) {
+	
+	int t = 0;
+  	float prob = (rand() % 100) *.01;
+  	int * coordPtr;
+  	float take_bresenham_prob = (use_chasing_prob) ? params->chasing_prob : 1.0;
+	
+	while (1) {
+
+		// get new line
+		findPathSofie(points, v->i, v->j, u->i, u->j);
+
+		// add probability to if boat V will take bresenham's move
+		if (prob < take_bresenham_prob) {
+			// 50% chance to take bresenham's line
+			v->i = points[1].i;
+			v->j = points[1].j;
+		} else {
+			// 50% chance to take random walk
+			// with all 8 directions, stay, and inv having 10% chance each
+			coordPtr = randomPoint(v->i, v->j, prob);
+			v->i = *coordPtr;
+			v->j = *(coordPtr + 1);
+		}
+
+		// take bresenham every time
+		//v->i = points[1].i;
+		//v->j = points[1].j;
+
+		// new prob?
+		prob = (rand() & 100) * .01;
+		
+		updateU(u, params->u_activity, t, prob);
+		
+		// check the zones 
+		v->region = inPolarRegion(v->i, v->j, u->i, u->j);
+		
+		printf("V: %d, %d, %c U: %d, %d, t: %d\n\n", v->i, v->j, v->region, u->i, u->j, t);
+		fprintf(params->fpt, "%d, %d, %d, %d\n", u->i, u->j, v->i, v->j);
+
+		// check for breaking 
+		if (t > params->maxsteps || (v->i == u->i && v->j == u->j)) {
+			printf("break   chasing prob: %.2f\n", take_bresenham_prob);
+			break;
+		}
+		
+		prob = (rand() & 100) * .01;
+		t++;
+	}
+}
