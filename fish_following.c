affea8b88c7024961ff8671ec4fa8a802c877015
eitanrait <exr644@miami.edu>
2021-07-23 13:05:13 -0400

    removed extraneous files: chasing.c following.c commands.txt ; added fish_following.c from sofie's repo, still need the file contents

---
+++ fish_following.c
@@ -0,0 +1,7 @@
+// copy over following
+// add time ticks so for k ticks do movement in one direction with noise
+// almost as if doing chasing until one point
+// then do randomwalk for k/2
+// then go to next point calculated randomly
+// remember to adjust to new bresenham as line goes on
+// could actually call chasing in chasing_prob.c with different points like from diver.c
